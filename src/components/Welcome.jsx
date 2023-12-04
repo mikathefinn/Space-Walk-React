e522d13e102f9e1dcebd98af836077d0a8d3806a
mikathefinn <mikathefinn@gmail.com>
2023-12-04 11:58:58 +0200

    images, headers

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Welcome() {
+  return (
+    <div className='main-headers'>
+        <h1 className='welcome-h1'>Welcome to</h1>
+        <h2 className='space-h2'>SpaceWalk</h2>
+    </div>
+  )
+}
+
+export default Welcome
