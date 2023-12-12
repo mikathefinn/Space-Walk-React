b13a26c169fbf7bfc350a18d9095ee653287469d
mikathefinn <mikathefinn@gmail.com>
2023-12-12 14:50:36 +0200

    context provider created

---
+++ src/Context/MarsContext.jsx
@@ -0,0 +1,15 @@
+import { createContext, useState, useEffect } from "react";
+
+const MarsContext = createContext()
+
+export const FeedbackProvider = ({children})=>{
+
+
+
+
+    return {
+        <MarsContext.Provider
+        value={{}}>{children}
+        </MarsContext.Provider>
+    }
+}
