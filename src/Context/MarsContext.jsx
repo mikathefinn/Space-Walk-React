675e71c476587c85a0204e486a80541baf22b657
mikathefinn <mikathefinn@gmail.com>
2023-12-14 08:29:28 +0200

    random image finally works

--- src/Context/MarsContext.jsx
+++ src/Context/MarsContext.jsx
@@ -6,36 +6,47 @@ const MarsContext = createContext()
 export const MarsProvider = ({ children }) => {
   const [slideshowImage, setSlideshowImage] = useState(placeholder)
 
-
   async function getAndDisplayImage() {
     try {
       const response = await fetch(
-        'https://mars-photos.herokuapp.com/api/v1/manifests/curiosity?api_key=ew009QLOkglDGuTtrFtdyQy8oCVTw2KI7gf4VOzQ'
+        'https://api.nasa.gov/mars-photos/api/v1/manifests/curiosity?api_key=ew009QLOkglDGuTtrFtdyQy8oCVTw2KI7gf4VOzQ'
       )
       const data = await response.json()
-      const latestSol = data.photo_manifest.max_sol
+      //filter through the photos to find ones with MAST camera
+      const filteredData = data.photo_manifest.photos.filter((item) =>
+        item.cameras.includes('FHAZ')
+      )
 
-      const randomSol = Math.floor(Math.random() * latestSol + 1)
+      const randomSol =
+        filteredData[Math.floor(Math.random() * filteredData.length + 1)].sol
 
       const photosResponse = await fetch(
         `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${randomSol}&api_key=ew009QLOkglDGuTtrFtdyQy8oCVTw2KI7gf4VOzQ`
       )
       const photosData = await photosResponse.json()
       //photosData.photos is an array of objects from a randomly selected sol
-      //accessing a random image object and retrieving it's source url
-      const randomImageObj =
-        photosData.photos[
-          Math.floor(Math.random() * photosData.photos.length + 1)
-        ]
-     setSlideshowImage(randomImageObj.img_src)
-      
+      //that has FHAZ images -- filter through to get only those images
+
+      const filteredPhotos = photosData.photos.filter(
+        (item) => item.camera.name === 'FHAZ'
+      )
+
+      //choose the source url of a random image from the filteredPhotos
+      const randomImgSrc =
+        filteredPhotos[
+          Math.floor(Math.random() * filteredPhotos.length + 1)
+        ].img_src
+
+     console.log(randomImgSrc);
+      setSlideshowImage(randomImgSrc)
     } catch (error) {
       console.log('error fetching data', error)
     }
   }
 
   return (
-    <MarsContext.Provider value={{ slideshowImage, setSlideshowImage, getAndDisplayImage }}>
+    <MarsContext.Provider
+      value={{ slideshowImage, setSlideshowImage, getAndDisplayImage }}>
       {children}
     </MarsContext.Provider>
   )
